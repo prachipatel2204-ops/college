9e318cb6a44119c4a83316b07c94af0a28931950
prachipatel2204-ops <prachipatel2204@gmail.com>
2025-12-24 12:39:39 +0530

    Folder Structure Created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Dashboard } from './dashboard/dashboard';
+import { Teacher } from './teacher/teacher';
+import { Student } from './student/student';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'dashboard',component:Dashboard},
+    {path:'',redirectTo:'dashboard',pathMatch:'full'},
+    {path:'teacher',component:Teacher},
+    {path:'student',component:Student}
+];
